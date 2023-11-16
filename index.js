a723649f800cad3ad6866cd4ce7f01ed207eb5e7
Tristan Gauci <tristan@maega.com.au>
2023-11-16 13:46:32 +1100

    Implement options

--- index.js
+++ index.js
@@ -3,23 +3,52 @@ import fs from 'fs';
 import tmp from 'tmp';
 import progress from 'progress-stream';
 import ProgressBar from 'cli-progress';
+import path from 'path';
+import ora from 'ora';
 
-export default async function downloadBinary(url) {
+const defaultOptions = {
+    startMsg: 'Starting download...',
+    downloadMsg: 'Downloading, please wait...',
+    successMsg: 'Download completed',
+    errorMsg: 'Download failed',
+    appendMissingExtension: true,
+    httpMethod: 'get',
+    showProgressBar: true
+}
+
+export default async function downloadFile(url, filePath = null, options = {}) {
+    // Merge the default options with the user options
+    options = { ...defaultOptions, ...options };
+
+    const preSpinner = ora(`\x1b[34m${options.startMsg}\x1b[0m`);
+    if (options.startMsg) preSpinner.start();
+    
     try {
-        const response = await axios.get(url, {
+        // Send a GET request to the url
+        const response = await axios({
+            url: url,
+            method: options.httpMethod,
             responseType: 'stream',
             maxRedirects: 5,
         });
 
+        // Get the file extension from the url
         const fileExtension = path.extname(new URL(url).pathname);
-        const tmpFile = tmp.tmpNameSync({ postfix: fileExtension });
 
-        const writer = fs.createWriteStream(tmpFile);
+        // If filePath is specified and appendMissingExtension is true, append the file extension to the filePath if it is missing
+        if (filePath && options.appendMissingExtension && !path.extname(filePath)) filePath += fileExtension;
+
+        // If filePath is falsy, create a temporary file
+        if (!filePath) filePath = tmp.tmpNameSync({ postfix: fileExtension });
+
+        const writer = fs.createWriteStream(filePath);
 
         var str;
-        if (response.headers['content-length'] !== undefined) {
+        if (response.headers['content-length'] !== undefined && options.showProgressBar) {
             // If the content-length header is present, use a progress bar
 
+            preSpinner.stop();
+            console.log(`\x1b[34m${options.downloadMsg}\x1b[0m`);
             const progressBar = new ProgressBar.SingleBar({}, ProgressBar.Presets.shades_classic);
             progressBar.start(parseInt(response.headers['content-length']), 0);
 
@@ -37,24 +66,39 @@ export default async function downloadBinary(url) {
             await new Promise((resolve, reject) => {
                 writer.on('finish', () => {
                     progressBar.stop();
-                    resolve(tmpFile);
+                    if (options.successMsg) console.log(`\x1b[32m✓ ${options.successMsg}\x1b[0m`);
+                    resolve(filePath);
                 });
-                writer.on('error', reject);
+                writer.on('error', (err) => {
+                    progressBar.stop();
+                    reject(err);
+                });
             });
         } else {
             // If the content-length header is not present, use a spinner
 
-            console.log(msg.bold("Downloading, please wait..."));
+            preSpinner.stop();
+            const spinner = ora(`\x1b[34m${options.downloadMsg}\x1b[0m`).start();
             response.data.pipe(writer);
 
             await new Promise((resolve, reject) => {
-                writer.on('finish', () => resolve(tmpFile));
-                writer.on('error', reject);
+                writer.on('finish', () => {
+                    options.successMsg
+                        ? spinner.succeed(`\x1b[32m${options.successMsg}\x1b[0m`)
+                        : spinner.stop();
+                    resolve(filePath);
+                });
+                writer.on('error', (err) => {
+                    spinner.stop();
+                    reject(err);
+                });
             });
         }
-        return tmpFile;
+        return filePath;
     } catch (err) {
-        console.log(msg.error('Error downloading file'));
-        console.error(err);
+        preSpinner.stop();
+        if (options.errorMsg) console.log(`\x1b[31m✗ ${options.errorMsg}\x1b[0m`);
+        throw err;
+        //throw new Error(err);
     }
 };
